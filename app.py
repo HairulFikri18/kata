abaa6742724ba251036be84277d3fb7ddffe1786
HairulFikri18 <hairulfikri187@gmail.com>
2023-06-04 17:16:52 +0700

    Update

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, request, render_template, redirect, url_for, jsonify
 from pymongo import MongoClient
 import requests
 from datetime import datetime
 from bson import ObjectId
 
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
 
 app = Flask(__name__)
-
-
-cxn_str = 'mongodb+srv://hairulfikri:palembang01@cluster0.7v3inet.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient(cxn_str)
-
-db = client.project02
 
 @app.route('/')
 def main():
